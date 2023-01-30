1e6bba32424ce6a586274b9e7679d34003f40b1c
amansoni <amansoni2041999@gmail.com>
2023-01-30 22:07:32 +0530

    welcome to Data structure program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProgram
+{
+    internal class OrderedList
+    {
+        static void Main(string[] args)
+        {
+            OrderedList orderedList = new OrderedList();
+
+            Console.WriteLine("Enter word to Search");
+            string word = Console.ReadLine();
+            orderedList.Ordered(word);
+        }
+    }
+}
